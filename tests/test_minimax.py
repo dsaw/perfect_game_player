ec95b4af35d639c89053d65f72b0b6628523183c
Devesh Sawant <devesh47cool@gmail.com>
2018-06-06 23:16:00 +0530

    test minimax added & Node tested

---
+++ tests/test_minimax.py
@@ -0,0 +1,25 @@
+import unittest
+
+import minimax_tree
+
+class TestMinimaxDS(unittest.TestCase):
+
+    def setUp(self):
+        self.node = minimax_tree.Node()
+
+    def tearDown(self):
+        pass
+
+
+    def test_eval_node(self):
+        '''
+
+        :return:
+        '''
+        self.assertIsNone(self.node.evaluate())
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+    
