7e1078647177a3050bf2f4c8f8204b3928b98ed9
Devesh Sawant <devesh47cool@gmail.com>
2018-09-02 20:29:41 +0530

    renamed repo name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-setup(name='Perfect game player',
-      version='1.0',
+setup(name='perfect_game_player',
+      version='1.1',
       description='Plays board games using Minimax algorithm',
       url='https://github.com/dsaw/perfect-game-player',
       author='Devesh Sawant',
