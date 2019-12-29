28157269b59c77ebc9d960d6b50f0710d7ef0369
devesh <devesh47cool@gmail.com>
2019-12-29 12:08:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='perfect_game_player',
       version='1.1',
@@ -6,6 +6,7 @@ setup(name='perfect_game_player',
       url='https://github.com/dsaw/perfect-game-player',
       author='Devesh Sawant',
       author_email='devesh47cool@gmail.com',
+      packages=find_packages(),
       license='GPLv3',
       install_requires=[],
       zip_safe=False
